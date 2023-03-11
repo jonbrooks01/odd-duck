036e33e9162c1af30f9ba4b01ed451cb08e790cd
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-03-11 13:42:25 -0800

    added assests

---
+++ js/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+// <!-- Global Variables -->
+let numVotes = 5;
+
+const state = {
+  allProductsArray: [],
+};
+
+// <!-- DOM References -->
+
+
+
+// <!-- CONSTRUCTOR function -->
+
+
+
+// <!-- Helper function -->
+
+
+
+// <!-- Event Handler -->
+
+
+
+// <!-- Listeners  -->
